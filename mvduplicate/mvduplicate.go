17bdb0fffa77a3f91ad3b740962f6cde72493bc9
imanau <ima_nau7@yahoo.co.jp>
2020-05-04 23:15:13 +0900

    Add: コンフィグファイルの読み込み処理を追加

--- mvduplicate/mvduplicate.go
+++ mvduplicate/mvduplicate.go
@@ -1,10 +1,26 @@
 package mvduplicate
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"path/filepath"
 )
 
-// ListFile 指定したディレクトリ以下の各ファイルのパスを返す
-func ListFile() {
-	fmt.Print("このpackageで重複ファイル削除を行います")
+// Configuration 設定ファイル読み込み用のstruct
+type Configuration struct {
+	Directory string `json:"directory"`
+}
+
+// ReadConfiguration configファイルを読み込む
+func ReadConfiguration(configPath string) *Configuration {
+	raw, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		fmt.Println(err.Error())
+
+	}
+	var config Configuration
+	json.Unmarshal(raw, &config)
+	config.Directory = filepath.FromSlash(config.Directory)
+	return &config
 }
